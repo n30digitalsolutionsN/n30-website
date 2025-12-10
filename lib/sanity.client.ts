07a07fb733af8e844a6941b1a6216e0ca8de718f
Elton Neo <eltonneo60@gmail.com>
2025-12-11 07:07:30 +1000

    Add default Sanity credentials fallback

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "9cz4jjd1";
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 const apiVersion = "2024-01-01";
 
 export const client = createClient({
